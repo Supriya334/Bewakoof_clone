a8ffe0d06898163a95b6c5a20da9070d9aa82bea
supriya <124588951+Supriya334@users.noreply.github.com>
2024-04-14 22:41:02 +0530

    Add files via upload

---
+++ prod.js
@@ -0,0 +1,56 @@
+
+let container = document.querySelector(".cont-child2")
+let arr = []
+
+const fetchData = async () => {
+    let url = `jsonData/men.json`
+
+    try {
+        let res = await fetch(url)
+
+        let data = await res.json()
+        arr= data
+
+        // console.log(arr)
+
+        displayData()
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const lowToHighBtn = document.getElementById('lowToHigh');
+    const highToLowBtn = document.getElementById('highToLow');
+    lowToHighBtn.addEventListener('click', () => {
+        arr.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+        displayData();
+    });
+    highToLowBtn.addEventListener('click', () => {
+        arr.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+        displayData();
+    });
+
+
+    
+function displayData() {
+    container.innerHTML = ""; 
+
+    arr.forEach((elem) => {
+        
+        let div = document.createElement("div")
+        div.innerHTML = `<img src=${elem.image} alt="">
+        <h3> ${elem.name} </h3>
+        <p>${elem.price}</p>
+        <p>${elem.discount}</p>`
+div.className="child-22"
+
+        container.append(div)
+    })
+
+
+}
+
+fetchData()
